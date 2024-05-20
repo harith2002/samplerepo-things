d75b5f8bbfcfaa4537eaecfdd153699cde453519
harith2002 <harith.hakim2002@gmail.com>
2024-05-21 01:40:21 +0800

    Test commit

---
+++ lol.java
@@ -0,0 +1,13 @@
+public class lol {
+    private int a;
+    private String s;
+
+    public lol(int a, String s) {
+        this.a = a;
+        this.s = s;
+    }
+
+    public int add(int b) {
+        return this.a + b;
+    }
+}
